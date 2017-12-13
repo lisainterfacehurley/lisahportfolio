23988c00a1f7ee0f9d8eff927887fedce4311ba7
lisainterfacehurley <30816052+lisainterfacehurley@users.noreply.github.com>
2017-12-13 09:14:16 -0600

    Big Corporate Project:  Largest Single Project in this Portfolio

---
+++ corporate/js/contact.php
@@ -0,0 +1,40 @@
+
+
+/*
+ * Script for sending E-Mail messages.
+ * 
+ */
+
+
+$sendTo = "lhurley2@cox.net";
+
+$action = $_POST['action'];
+
+    $name = $_POST['form'][0]['name'];
+    $email = $_POST['form'][0]['email'];
+    $subject = $_POST['form'][0]['subject'];
+    $message = $_POST['form'][0]['message'];
+
+    if ($name == "" || $email == "" || $message == "" || $subject == "") {
+        echo "<p class=\"error\">There was problem while sending E-Mail. Please verify entered data and try again!</p>";
+        exit();
+    }
+
+    else
+    {
+        $header = 'From: ' . $name . '<' . $email . ">\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+        $sent = mail($sendTo, $subject, $message, $header);
+
+        if ($sent) {
+            echo "<p class=\"success\">Message sent successfully.</p>";
+        } else {
+            echo "<p class=\"error\">There was problem while sending E-Mail.</p>";
+        }
+    }
+
+
+
+
